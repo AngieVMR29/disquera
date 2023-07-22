8045540c435d80cfd3a085c9b0e5ba6d10e55d47
sena <gtrkevin34@gmail.com>
2023-07-22 11:58:04 -0500

    migrar datos a base

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Disquera(models.Model):
+    id=models.AutoField(primary_key=True)
+    nitdisquera=models.CharField(max_length=25)
+    nombredisquera=models.CharField(max_length=100)
+    telefono=models.CharField(max_length=15)
+    direccion=models.CharField(max_length=100)
+    estadoDisquera=models.BooleanField()
+    
+class Artista(models.Model):
+    id=models.AutoField(primary_key=True)
+    iddisquerakf= models.ForeignKey(Disquera,on_delete=models.CASCADE)
+    
